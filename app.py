4eacbe41247413c06bdc5ba86e05ab4151ebd998
muhammad noman siddiqui <nk5461420@gmail.com>
2024-12-23 20:15:07 +0500

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
-app = Flask(_name_)
+app= Flask(__name__)
 
-@app.route("/")
+app.route("/")
 def home():
-    return render_template("index.html")
+    return "Hello, Flask!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
